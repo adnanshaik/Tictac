60963e7346f393260cc97ff6e1f9e3490e2be2dd
adnanshaik <shaikhadnan6608@gmail.com>
2020-12-03 11:58:03 +0530

    second case

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,7 +1,8 @@
+
 import java.util.*;
 import java.lang.*;
 
-public class TicTactoe
+public class TicTac
 {
 
  public static  char[] createBoard()
@@ -14,14 +15,38 @@ public class TicTactoe
         return board;
      }
 
-  
+        public static String userData()
+        {
+
+                Scanner sc = new Scanner(System.in);
+                System.out.println("entr x or o");
+
+                String User=sc.nextLine();
+                if(User=="X")
+                {
+                 return User;
+                }
    }
 
 public static void main(String args[])
 {
-     
+         static String User=0;
+         static String Bot=0;
         char[] board=createBoard();
+        String Answer=userData();
+
+        if(Answer=="X")
+        {
+                User="X";
+                 Bot="O";
+        }
+        else
+        {
+                Bot="X";
+                User="O"
+        }
+        System.out.println(Answer);
 }
 
-
+}
 
