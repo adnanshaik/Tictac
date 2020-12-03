ca34a4adc0c58353f41ba08da09463600222b5dc
adnanshaik <shaikhadnan6608@gmail.com>
2020-12-03 14:02:17 +0530

    fourth commit

--- Tictactoe.java
+++ Tictactoe.java
@@ -5,15 +5,15 @@ import java.lang.*;
 public class Tictactoe
 {
 
- public static  char[] createBoard()
-     {
-        char board[] = new char [10];
-          for(int i=1;i<=board.length;i++)
-          {
+	 public static  char[] createBoard()
+     	{
+           char board[] = new char [10];
+            for(int i=1;i<=board.length;i++)
+           {
              board[i]=' ';
-          }
-        return board;
-     }
+           }
+           return board;
+        }
 
         public static String userData()
         {
@@ -21,25 +21,52 @@ public class Tictactoe
                Scanner sc = new Scanner(System.in);
                System.out.println("entr x or o");
 
-                String User=sc.nextLine();
-                if(User=="X")
+                String data=sc.nextLine();
+                if(data.equals("X"))
                 {
-                 return User;
+                 return data;
                 }
         }
 
-	public static void displayGame(char show[])
+	public static void displayGame()
 	{
-		int count=0;
-		for(int k=1;k<=3;k++)
-		{
-		 for(int i=1;i<=show.length-1;i++)
-		 {
-			count+=count;
-			System.out.println(show[count]);
-		 }
-		}
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[1] + " | "
+                           + board[2] + " | " + board[3]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[4] + " | "
+                           + board[5] + " | " + board[6]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[7] + " | "
+                           + board[8] + " | " + board[9]
+                           + " |");
+        System.out.println("|---|---|---|");
+         }
 	}
+
+
+	public static void moveUser(char[] game,String user)
+	{
+		System.out.println("enter 0-9");
+		Scanner n = new Scanner(System.in);
+		int num=n.nextInt();
+		if(num>9)
+		{
+		System.out.println("enter again");
+		}
+		else if(game[num]==' ')
+			{
+			   game[num]=user.charAt(0);
+			}
+		else
+			{
+			System.out.println("enter again");
+			displayGame(board);
+			}
+
+
 }
 
 public static void main(String args[])
@@ -49,7 +76,7 @@ public static void main(String args[])
         char[] board=createBoard();
         String Answer=userData();
 
-        if(Answer=="X")
+        if(Answer.equals("X"))
         {
                 User="X";
                  Bot="O";
@@ -61,8 +88,8 @@ public static void main(String args[])
         }
 
       	displayGame(board);
+	moveUser(board,Answer);
 
 }
-
 }
 
